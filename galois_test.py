e1f7a174b89d263110994548503777f6ebe7e017
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-12-22 13:48:50 +0300

    add tests for polynomial multiplication

--- galois_test.py
+++ galois_test.py
@@ -1,5 +1,6 @@
 from galois_field import GF
 
+
 def test_div():
     assert GF(3, 3).poly_div([1, 1, 0, 2], [2]) == ([2, 2, 0, 1], [0])
 
@@ -8,3 +9,9 @@ def test_div():
     assert GF(5, 3).poly_div([1, 1, 0, 0, 1, 1], [1, 1]) == ([1, 0, 0, 0, 1], [0])
 
     assert GF(2, 3).poly_div([1, 0, 0, 0], [1, 0, 1, 1]) == ([1], [1, 1])
+
+
+def test_mult():
+    assert GF(2, 3).poly_mult([0], [1, 1, 1, 1, 1, 0, 0]) == [0]
+    assert GF(2, 3).poly_mult([1, 0], [1, 0, 0]) == [1, 1]
+    assert GF(2, 3).poly_mult([1, 1, 0], [1, 1, 0]) == [1, 0]
